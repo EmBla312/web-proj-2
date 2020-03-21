72d89b6710d3d34bc08bffa3ebfa7bd4ecd9a3cf
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2020-03-21 13:53:55 -0500

    updated food page

---
+++ scripts/tabs.js
@@ -0,0 +1,13 @@
+function openContent(evt, content) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabs");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(content).style.display = "block";
+    evt.currentTarget.className += " active";
+}
