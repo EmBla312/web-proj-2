17d7e05c1f1a7611d8d01de683a638d43023917c
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2020-03-23 20:18:16 -0500

    added tab functionality

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,13 +1,30 @@
 function openContent(evt, content) {
-    var i, tabcontent, tablinks;
+    var i, tabcontent, tabs;
+
     tabcontent = document.getElementsByClassName("tab-content");
+    console.log("TABCONTENT");
+    console.log(tabcontent);
+
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tabs");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+
+    tabs = document.getElementsByClassName("tabs");
+    console.log("TABS");
+    console.log(tabs);
+
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].className = tabs[i].className.replace(" active", "");
     }
+    
+    console.log("BEFORE:");
+    console.log(document.getElementById(content));
+
     document.getElementById(content).style.display = "block";
     evt.currentTarget.className += " active";
+
+    console.log("AFTER:");
+    console.log(document.getElementById(content));
+
 }
+
