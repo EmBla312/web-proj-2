bd1286443d03fa89e62989a554008fbb318c42b3
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2020-05-04 15:50:23 -0500

    deploy test2

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded());
-app.use(express.static(path.join(__dirname+'/pages')));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'index.html'))
 });
 
 app.listen(PORT, () => {
